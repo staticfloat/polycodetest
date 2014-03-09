f9af711ca64302f36d7d642cac51b16db35fe62c
Elliot Saba <staticfloat@gmail.com>
2014-03-08 17:16:25 -0800

    Initial Commit

---
+++ polycodetest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  polycodetest
+//
+//  Created by Elliot Saba on 3/8/14.
+//  Copyright (c) 2014 Elliot Saba. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ polycodetest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  polycodetest
+//
+//  Created by Elliot Saba on 3/8/14.
+//  Copyright (c) 2014 Elliot Saba. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
